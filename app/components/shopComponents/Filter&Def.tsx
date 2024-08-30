1ce717e3047334cef29bbf07b731a59dde0bc22e
Daniel Adetaiwo <danieladetaiwoeniola@gmail.com>
2024-08-30 14:29:11 +0100

    Ui

--- app/components/shopComponents/Filter&Def.tsx
+++ app/components/shopComponents/Filter&Def.tsx
@@ -5,7 +5,7 @@ import Filterdrop from './Filterdrop'
 const font = localFont({src:"../../fonts/dd.woff2"})
 function Filters() {
   return (
-    <div className='flex py-[1em] sticky  shadow-sm top-[10.2em] px-[1em] gap-[2em] w-full'>
+    <div className='flex items-center sticky  z-[60] bg-white h-[3.9875em] shadow-sm top-[6.6em] px-[1em] gap-x-[2em] w-full'>
       <Filterbtn/>
       <Filterdrop/>
     </div>
--- app/components/shopComponents/Filterbtn.tsx
+++ app/components/shopComponents/Filterbtn.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const myFont = localFont({src:"../../fonts/dd.woff2"})
 function Filterbtn() {
   return (
-    <button className={`${myFont.className} flex items-center gap-[0.2em] underline-offset-2 text-[0.875em] text-[#626262] font-[500] tracking-[1px] underline`} type='button'>
+    <button className={`${myFont.className} flex items-center gap-x-[0.2em] underline-offset-2 text-[0.875em] text-[#626262] font-[500] tracking-[1px] underline`} type='button'>
       <Image src="/filter.svg" width={500} className='w-[1em] h-[1em]' height={300} alt='filter'/>
       FILTERS(0)
     </button>
