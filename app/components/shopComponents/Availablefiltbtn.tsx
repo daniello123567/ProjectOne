0a93d15b81b54d582c452090c8c8c6f05d6a22de
Daniel Adetaiwo <danieladetaiwoeniola@gmail.com>
2024-09-06 00:41:18 +0100

    kki

--- app/components/shopComponents/Availablefiltbtn.tsx
+++ app/components/shopComponents/Availablefiltbtn.tsx
@@ -14,7 +14,7 @@ function Availablefiltbtn({filter,keyVal}:{filter:string,keyVal:string}) {
     replace(`?${params}`,{scroll:false})
   }
   return (
-    <button onClick={handleDeletefromUrl} type='button' className={` ${shoptitlefont1.className} h-[1.875em] flex items-center text-[0.75em] font-[400] rounded-full w-max outline outline-1 outline-black py-[.25em] px-[0.5em]`}>{filter}
+    <button onClick={handleDeletefromUrl} type='button' className={` ${shoptitlefont1.className} h-[1.875em] flex items-center text-[0.75em] font-[400] rounded-full w-max border border-black outline-1 outline-black py-[.25em] px-[0.5em]`}>{filter}
         <Image className='w-[1em] h-[1em]' src="/Cross.svg" alt='cancel' width={500} height={500}/>
       </button>
   )
