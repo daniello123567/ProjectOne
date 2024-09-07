9c00bc6f0bbf3328d661cba94740be1b14853b39
Daniel Adetaiwo <danieladetaiwoeniola@gmail.com>
2024-09-07 15:40:04 +0100

    bag up

--- app/components/Bag.tsx
+++ app/components/Bag.tsx
@@ -27,14 +27,14 @@ const variants = {
  }
 }
 function BagShop() {
-  const {setBagVisibility,activePage,setactivePage} = useContext(globalStore);
+  const {setBagVisibility,activePage,setactivePage,Bag} = useContext(globalStore);
 
   return (
     <div className='fixed bottom-0 z-[99999] lg:pt-0 pt-[4em] bg-white/50  backdrop-blur-sm w-full h-screen'>
       <motion.div exit="exit" initial="stayDown" variants={variants} animate="comeUp" className='bg-white lg:w-[40%] lg:h-full lg:rounded-none border-t  pt-[4em] relative rounded-t-[1em] h-full w-full'>
         <Image onClick={()=>setBagVisibility(false)} className='w-[1.5em] cursor-pointer absolute top-[1em] right-[1em]  h-[1.5em]' src="/Cancel.svg" width={500} height={500} alt='cancel'/>
         <div className='w-full flex border-b border-b-black justify-between h-[1.84375em]'>
-          <button onClick={()=>setactivePage('Bag')} type='button' className={`h-full font-[400] pb-[0.25em] ${activePage=="Bag"? Active.className  + ' border-b-2 font-[500] border-b-black ': nonActive.className} w-[50%]`}>Bag</button>
+          <button onClick={()=>setactivePage('Bag')} type='button' className={`h-full font-[400] pb-[0.25em] ${activePage=="Bag"? Active.className  + ' border-b-2 font-[500] border-b-black ': nonActive.className} w-[50%]`}>{'Bag' + `(${Bag.length})`}</button>
           <button  onClick={()=>setactivePage('Wishlist')} type='button' className={`h-full font-[400] pb-[0.25em] ${activePage=="Wishlist"? Active.className + ' border-b-2 font-[500] border-b-black': nonActive.className } w-[50%]`}>Wishlist</button>
         </div>
         {activePage==="Bag"&&<BagInBag/>}
