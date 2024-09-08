f49af81b266bb67b58e705dc87277f89c748ddc5
Daniel Adetaiwo <danieladetaiwoeniola@gmail.com>
2024-09-08 19:11:31 +0100

    kk

--- app/previewbag/page.tsx
+++ app/previewbag/page.tsx
@@ -1,19 +1,18 @@
-"use client"
+import Image from 'next/image'
 import React from 'react'
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
-function Page() {
-  return (
-    <div className='grid md:grid-cols-3 gap-[1em] h-screen lg:grid-cols-4 lg:grid-rows-4 md:grid-rows-3  grid-cols-2 grid-rows-2'>
-    <Skeleton height={"100%"} inline={false} width={"100%"}/>
-    <Skeleton height={"100%"} inline={false} width={"100%"}/>
-    <Skeleton height={"100%"} inline={false} width={"100%"}/>
-    <Skeleton height={"100%"} inline={false} width={"100%"}/>
-    <Skeleton height={"100%"} inline={false} width={"100%"}/>
-    <Skeleton height={"100%"} inline={false} width={"100%"}/>
-     </div>
 
+function page() {
+
+  return (
+    <div className='w-full px-[1em] relative pt-[3em] border h-screen bg-white rounded-t-[1em]'>
+     <Image className='w-[1.7em] absolute right-[1em] top-[1em] h-[1.7em]' src="/Cancel.svg" alt='cancel' width={500} height={500}/>
+
+     <div className='w-full relative h-[2.5625em]'>
+      <input placeholder='Search for any products' className={`h-full placeholder: px-[2em] border-b border-b-black outline-none w-full`} title='searchquery' type='text'/>
+      <Image alt='search' className='w-[1.2em] absolute top-[.6em] h-[1.2em]' width={500} height={500} src="Search.svg"/>
+     </div>
+    </div>
   )
 }
 
-export default Page
+export default page
