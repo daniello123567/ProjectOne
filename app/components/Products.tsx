e44754710a6504c213419d9e7f6cf9c843db738c
Daniel Adetaiwo <danieladetaiwoeniola@gmail.com>
2024-09-10 11:29:06 +0100

    flstop

--- app/components/Products.tsx
+++ app/components/Products.tsx
@@ -30,7 +30,9 @@ function Products() {
     <div className='grid lg:gap-[1em] h-full md:grid-cols-3 lg:grid-cols-4 lg:grid-rows-4 md:grid-rows-3 px-[1em] gap-[.7em]  grid-cols-2 grid-rows-2'>
 
        {
-        isPending?<ProductSkeletons/>:
+        isPending?
+        <ProductSkeletons/>
+        :
         <Allproducts/>
        }
        </div>
