fe37208d76d35a09c8d7b706902ef78d7c22e62d
Daniel Adetaiwo <danieladetaiwoeniola@gmail.com>
2024-09-16 18:25:06 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-
+export const metadata:Metadata = {
+  title:"SHOP BEST JEWELRIES|NUBE",
+  icons:"/logo.png",
+  description:"Nube the best place to shop all your jewelries"
+}
 export default function RootLayout({
   children,
 }: Readonly<{
