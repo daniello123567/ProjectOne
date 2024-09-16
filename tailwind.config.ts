fea7964febf7d0ec10593d1cfb049d7fea01f8fb
Daniel Adetaiwo <danieladetaiwoeniola@gmail.com>
2024-09-16 21:32:25 +0100

    test

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,12 +26,12 @@ const config: Config = {
     extend: {
       keyframes:{
         fadeIn:{
-          '0%': { opacity: '0' },
+          '0%': { opacity: '0.7' },
           '100%': { opacity: '1' },
         }
       },
       animation:{
-        fadeIn:"fadeIn 0.5s ease-in-out"
+        fadeIn:"fadeIn 0.9s ease-in-out"
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
